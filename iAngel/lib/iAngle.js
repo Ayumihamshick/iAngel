97de8946fb92cc87cbdfdf6657bc9f380c3588c9
Jack Sparrow <frostmourner@outlook.com>
2016-11-18 15:51:35 +0800

    161118——1551
    be created

---
+++ iAngel/lib/iAngle.js
@@ -0,0 +1,107 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var iAngel;
+(function (iAngel) {
+    var Utils = (function () {
+        function Utils() {
+        }
+        Utils.getClassName = function (clazz) {
+            return (clazz.toString().replace(/function\s?/mi, "").split("("))[0];
+        };
+        return Utils;
+    }());
+    iAngel.Utils = Utils;
+    /**
+     * U can use it create angularJS Object. like .controller('|N|name',['|P|$scope',|F|function($scope){...}])
+     * The Class Name  => |N|
+     */
+    var Nut = (function () {
+        /**
+         * @param module module = angular.module, if U don't have any TypeScript about angular.module U can use type any.
+         */
+        function Nut(module) {
+            var thas = this;
+            this._module = module;
+            var arr = this.getParams();
+            arr[arr.length] = function () {
+                var params = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    params[_i - 0] = arguments[_i];
+                }
+                thas.onCreate.apply(thas, [this].concat(params));
+            };
+            this._init(arr, module);
+        }
+        Object.defineProperty(Nut.prototype, "module", {
+            /**
+             * return module.
+             */
+            get: function () {
+                return this._module;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Nut;
+    }());
+    var Config = (function (_super) {
+        __extends(Config, _super);
+        function Config() {
+            _super.apply(this, arguments);
+        }
+        Config.prototype._init = function (arr, module) {
+            module.config(arr);
+        };
+        return Config;
+    }(Nut));
+    iAngel.Config = Config;
+    var Directive = (function (_super) {
+        __extends(Directive, _super);
+        function Directive() {
+            _super.apply(this, arguments);
+        }
+        Directive.prototype.getName = function () {
+            return this._name;
+        };
+        Directive.prototype._init = function (arr, module) {
+            this._name = Utils.getClassName(this.constructor);
+            module.directive(this.getName(), arr);
+        };
+        return Directive;
+    }(Nut));
+    iAngel.Directive = Directive;
+    var Controller = (function (_super) {
+        __extends(Controller, _super);
+        function Controller() {
+            _super.apply(this, arguments);
+        }
+        Controller.prototype.getName = function () {
+            return this._name;
+        };
+        Controller.prototype._init = function (arr, module) {
+            this._name = Utils.getClassName(this.constructor);
+            module.controller(this.getName(), arr);
+        };
+        return Controller;
+    }(Nut));
+    iAngel.Controller = Controller;
+    var Service = (function (_super) {
+        __extends(Service, _super);
+        function Service() {
+            _super.apply(this, arguments);
+        }
+        Service.prototype.getName = function () {
+            return this._name;
+        };
+        Service.prototype._init = function (arr, module) {
+            this._name = Utils.getClassName(this.constructor);
+            module.service(this.getName(), arr);
+        };
+        return Service;
+    }(Nut));
+    iAngel.Service = Service;
+})(iAngel || (iAngel = {}));
+//# sourceMappingURL=iAngle.js.map
---
+++ iAngel/lib/iAngle.ts
@@ -0,0 +1,105 @@
+﻿module iAngel {
+    export class Utils {
+        public static getClassName(clazz): string {
+            return (clazz.toString().replace(/function\s?/mi, "").split("("))[0];
+        }
+    }
+
+    interface iClassName {
+        /**
+         * return name => |N|
+         */
+        getName(): string;
+    }
+
+    /**
+     * U can use it create angularJS Object. like .controller('|N|name',['|P|$scope',|F|function($scope){...}])
+     * The Class Name  => |N|
+     */
+    abstract class Nut<T> {
+        /**
+         * return angularJS's params. => |P|
+         */
+        abstract getParams(): Array<string>;
+        /**
+         * when the controller be create will use it. =>|F|
+         * @param thas this is an angularJS object;
+         * @param params => |P|
+         */
+        abstract onCreate(thas: any, ...params: Array<any>);
+
+        /**
+         * don't use it.....
+         * @param arr
+         * @param module
+         */
+        abstract _init(arr: Array<any>, module: T): void;
+
+        private _module: T;
+        /**
+         * return module.
+         */
+        get module(): T {
+            return this._module;
+        }
+        /**
+         * @param module module = angular.module, if U don't have any TypeScript about angular.module U can use type any.
+         */
+        constructor(module: T) {
+            var thas = this;
+            this._module = module;
+            var arr: Array<any> = this.getParams();
+            arr[arr.length] = function (...params: Array<any>) {
+                thas.onCreate(this, ...params);
+            }
+            this._init(arr, module);
+        }
+    }
+
+    export abstract class Config<T> extends Nut<T> {
+        abstract getParams(): Array<string>;
+        abstract onCreate(thas: Config<T>, ...params: Array<any>): void;
+        _init(arr, module) {
+            module.config(arr);
+        }
+    }
+
+    export abstract class Directive<T> extends Nut<T> implements iClassName {
+        abstract getParams(): Array<string>;
+        abstract onCreate(thas: any, ...params: Array<any>): Object;
+        private _name: string;
+        getName(): string {
+            return this._name;
+        }
+        _init(arr, module) {
+            this._name = Utils.getClassName((<any>this).constructor);
+            module.directive(this.getName(), arr);
+        }
+    }
+
+    export abstract class Controller<T> extends Nut<T> implements iClassName {
+        abstract getParams(): Array<string>;
+        abstract onCreate(thas: any, ...params: Array<any>): void;
+        private _name: string;
+        getName(): string {
+            return this._name;
+        }
+        _init(arr, module) {
+            this._name = Utils.getClassName((<any>this).constructor);
+            module.controller(this.getName(), arr);
+        }
+    }
+
+    export abstract class Service<T> extends Nut<T> implements iClassName {
+        abstract getParams(): Array<string>;
+        abstract onCreate(thas: any, ...params: Array<any>): void;
+        private _name: string;
+        getName(): string {
+            return this._name;
+        }
+        _init(arr, module) {
+            this._name = Utils.getClassName((<any>this).constructor);
+            module.service(this.getName(), arr);
+        }
+    }
+}
